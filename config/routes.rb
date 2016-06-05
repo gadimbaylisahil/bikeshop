4f43293c590b7184f1394686fda08052ebc4ab5c
Sahil <sahil.gadimbay@gmail.com>
2016-06-05 15:52:51 +0200

    controller for orders

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ Rails.application.routes.draw do
   # You can have the root of your site routed with "root"
   root 'static_pages#index'
 
+  resources :orders, only: [:index, :show, :create, :destroy]
+
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
