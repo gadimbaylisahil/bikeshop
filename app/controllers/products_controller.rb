5e86ee0f74d1a267c6041838b357c51c525ff6e2
Sahil <sahil.gadimbay@gmail.com>
2016-06-09 00:44:43 +0200

    navigation search box, more styling

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -4,7 +4,14 @@ class ProductsController < ApplicationController
   # GET /products
   # GET /products.json
   def index
-    @products = Product.all
+    if params[:q]
+      search_term = params[:q]
+      @products = Product.where("name LIKE ?", "%#{search_term}")
+      #return our filtered list
+    else
+      @products = Product.all
+      #returns all products
+    end
   end
 
   # GET /products/1
