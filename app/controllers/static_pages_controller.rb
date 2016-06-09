a79acc3ccd66ffe731b07752055853a113f6374f
Sahil <sahil.gadimbay@gmail.com>
2016-06-09 03:54:08 +0200

    testing heroku mail plugin

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,14 @@ class StaticPagesController < ApplicationController
   def shop
   	@products = Product.all
   end
+  def thank_you
+  	@name = params[:name]
+  	@email = params[:email]
+  	@message = params[:message]
+  	ActionMailer::Base.mail(
+  		:from => @email,
+  		:to => 'sahil.gadimbay@gmail.com', 
+  		:subject => "A new message from #{@name}", 
+  		:body => @message).deliver_now
+  end
 end
