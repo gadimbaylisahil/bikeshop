27d055597623b2457a261ae907fbfc21023c38de
Sahil <sahil.gadimbay@gmail.com>
2016-06-21 04:01:26 +0200

    sign-up email

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -10,4 +10,11 @@ class UserMailer < ApplicationMailer
 				)
 		@name = name
 	end
+
+	def welcome(user)
+		@appname = "Bike Shop"
+		mail(
+				:to => user.email,
+				:subject => "Welcome to #{@appname}!")
+	end
 end
