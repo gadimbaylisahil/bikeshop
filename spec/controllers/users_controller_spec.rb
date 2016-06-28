211cf774380f8dbfc38217265d003116b64d634b
Sahil <sahil.gadimbay@gmail.com>
2016-06-29 01:34:20 +0200

    some functional tests for user_controller

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,54 @@
+require 'rails_helper'
+
+describe UsersController, :type => :controller do 
+
+	before do 
+		@user = User.create!(:email => "test@test.com", :password => "123456")
+		@user2 = User.create!(:email => "test2@test.com", :password => "1234567")
+	end
+
+	describe "GET #show" do
+
+		context "user is logged in" do
+
+			before do 
+				sign_in @user
+			end
+
+			context "loads correct user details" do
+
+				before do 
+					get :show, :id => @user.id
+				end
+
+				it "responds successfully with an HTTP 200 status code" do
+	      			expect(response).to be_success
+	      			expect(response).to have_http_status(200)
+	    		end
+
+	    		it "assigned user is created user" do
+	    			expect(assigns(:user)).to eq @user
+	    		end
+	    	end
+
+	    	context "another user" do
+	    		it "redirects to login page" do 
+	    			get :show, :id => @user2.id
+	    			expect(response).to redirect_to(root_path)
+	    		end
+	    	end
+	    end
+
+
+		context "user is not logged in" do
+
+			it "redirects to login" do
+				get :show, id: @user.id 
+				expect(response).to redirect_to(root_path)
+			end
+
+		end
+
+	end
+
+end
