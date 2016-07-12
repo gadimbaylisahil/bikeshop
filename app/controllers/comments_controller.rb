20a6b46027ff2582a98d94a6d7ca30fa37b5d51e
Sahil <sahil.gadimbay@gmail.com>
2016-07-13 02:56:39 +0300

    ajax on comments + redirections fixed

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -6,22 +6,23 @@ class CommentsController < ApplicationController
   	@comment.user = current_user
   	respond_to do |format|
       if @comment.save
-        format.html { redirect_to @product, notice: 'Review was created successfully.' }
+        format.html { redirect_to :back, notice: 'Review was created successfully.' }
         format.json { render :show, status: :created, location: @product }
+        format.js
       else
-        format.html { redirect_to @product, alert: 'Review was not saved successfully. Please fill out all fields' }
+        format.html { redirect_to :back, alert: 'Review was not saved successfully. Please fill out all fields' }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
       end
     end
   end
 
   def destroy
-    if signed_in? && current_user.admin?
+    if signed_in? && current_user.admin? 
       @comment = Comment.find(params[:id])
       product = @comment.product
       @comment.destroy
     end
-    redirect_to product
+    redirect_to :back
   end
 
 	private
